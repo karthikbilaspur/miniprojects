67725706be9c00bd5d5d749e9c6417a24bb17213
V.Karthik <36740831+karthikbilaspur@users.noreply.github.com>
2024-05-14 00:08:10 +0530

    commit	modified:   public/index.html

---
+++ src/components/CaloryTracker/App.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import CaloryMeter from "./CaloryMeter"; 
+  
+function App() { 
+  return ( 
+    <div className="bg-blue-100 min-h-screen"> 
+      <CaloryMeter /> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
